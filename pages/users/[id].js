96ddd86fde0c37179dc1c500e1be072c511eaa11
Mohammadamin Sajedian <mohammadsajedian@gmail.com>
2022-03-26 21:51:46 +0000

    getStaticPaths added to the id pages

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,7 +1,21 @@
-const Details = () => {
-    return ( <div>
-        Details Page
-    </div> );
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await res.json()
+    const paths = data.map((user) => {
+        return {
+            params: { id: user.id.toString() }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
 }
- 
+
+const Details = () => {
+    return (<div>
+        Details Page
+    </div>);
+}
+
 export default Details;
